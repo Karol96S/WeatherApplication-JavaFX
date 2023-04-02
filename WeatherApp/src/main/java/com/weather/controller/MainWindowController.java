4df2495af37159cb34f6d50dfbd5e42453effd45
Karol_S <szypa288@gmail.com>
2023-04-03 01:00:01 +0200

    checked icon resizability resolution

--- WeatherApp/src/main/java/com/weather/controller/MainWindowController.java
+++ WeatherApp/src/main/java/com/weather/controller/MainWindowController.java
@@ -2,15 +2,19 @@ package com.weather.controller;
 
 import com.weather.WeatherMenager;
 import com.weather.model.Connector;
+import com.weather.model.Date;
 import com.weather.model.WeatherData;
 import com.weather.views.MainWindow;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.time.DayOfWeek;
 import java.util.List;
@@ -67,9 +71,10 @@ public class MainWindowController extends BaseController implements Initializabl
     @FXML
     void checkWeatherButtonAction() throws IOException, InterruptedException {
         startCityDisplayLabel.setText(startCity.getText());
-        String uriStart = Connector.buildApiRequest(startCity.getText());
-        String rawDataStart = Connector.sendRequest(uriStart);
-        WeatherData weatherDataStartCity = new WeatherData(rawDataStart);
+        updateStartingCityForecast();
+//        String uriStart = Connector.buildApiRequest(startCity.getText());
+//        String rawDataStart = Connector.sendRequest(uriStart);
+//        WeatherData weatherDataStartCity = new WeatherData(rawDataStart);
 
 
         destinationCityDisplayLabel.setText(destinationCity.getText());
@@ -77,10 +82,26 @@ public class MainWindowController extends BaseController implements Initializabl
         String rawDataDestination = Connector.sendRequest(uriDestination);
         WeatherData weatherDataDestinationCity = new WeatherData(rawDataDestination);
 
+//        List<Double> startCityDayTemperatures = weatherDataStartCity.getCityMaxTemperatures();
+//        List<DayOfWeek> daysOfTheWeek = weatherDataStartCity.getDays();
+//        tempStartDayOne.setText(startCityDayTemperatures.get(0).toString());
+//        dayOne.setText(Date.translatedDayOfTheWeek(daysOfTheWeek.get(0)));
+//        Image image = new Image(getClass().getResourceAsStream("/icons/clear-day.png"));
+//        dayOneStartImage.setImage(image);
+
+    }
+
+    private void updateStartingCityForecast() throws IOException, InterruptedException {
+        String uriStart = Connector.buildApiRequest(startCity.getText());
+        String rawDataStart = Connector.sendRequest(uriStart);
+        WeatherData weatherDataStartCity = new WeatherData(rawDataStart);
+
         List<Double> startCityDayTemperatures = weatherDataStartCity.getCityMaxTemperatures();
         List<DayOfWeek> daysOfTheWeek = weatherDataStartCity.getDays();
         tempStartDayOne.setText(startCityDayTemperatures.get(0).toString());
-        dayOne.setText(daysOfTheWeek.get(0).toString());
+        dayOne.setText(Date.translatedDayOfTheWeek(daysOfTheWeek.get(0)));
+        Image image = new Image(getClass().getResourceAsStream("/icons/clear-day.png"));
+        dayOneStartImage.setImage(image);
     }
 
     public void initialize(URL url, ResourceBundle resourceBundle) {
