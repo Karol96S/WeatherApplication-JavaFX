2e33c1da17d6df33e2db5acb281c9ffe34e5758c
Karol_S <szypa288@gmail.com>
2023-04-03 18:27:35 +0200

    temperatures show correctly for start city

--- WeatherApp/src/main/java/com/weather/controller/MainWindowController.java
+++ WeatherApp/src/main/java/com/weather/controller/MainWindowController.java
@@ -8,10 +8,14 @@ import com.weather.views.MainWindow;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 import java.io.FileInputStream;
@@ -50,37 +54,10 @@ public class MainWindowController extends BaseController implements Initializabl
     void checkWeatherButtonAction() throws IOException, InterruptedException {
         startCityDisplayLabel.setText(startCity.getText());
         updateStartingCityForecast();
-//        String uriStart = Connector.buildApiRequest(startCity.getText());
-//        String rawDataStart = Connector.sendRequest(uriStart);
-//        WeatherData weatherDataStartCity = new WeatherData(rawDataStart);
-
 
         destinationCityDisplayLabel.setText(destinationCity.getText());
-        String uriDestination = Connector.buildApiRequest(startCity.getText());
-        String rawDataDestination = Connector.sendRequest(uriDestination);
-        WeatherData weatherDataDestinationCity = new WeatherData(rawDataDestination);
-
-//        List<Double> startCityDayTemperatures = weatherDataStartCity.getCityMaxTemperatures();
-//        List<DayOfWeek> daysOfTheWeek = weatherDataStartCity.getDays();
-//        tempStartDayOne.setText(startCityDayTemperatures.get(0).toString());
-//        dayOne.setText(Date.translatedDayOfTheWeek(daysOfTheWeek.get(0)));
-//        Image image = new Image(getClass().getResourceAsStream("/icons/clear-day.png"));
-//        dayOneStartImage.setImage(image);
-
+        updateDestinationCityForecast();
     }
-
-//    private void updateStartingCityForecastBETA() throws IOException, InterruptedException {
-//        String uriStart = Connector.buildApiRequest(startCity.getText());
-//        String rawDataStart = Connector.sendRequest(uriStart);
-//        WeatherData weatherDataStartCity = new WeatherData(rawDataStart);
-//
-//        List<Double> startCityDayTemperatures = weatherDataStartCity.getCityMaxTemperatures();
-//        List<DayOfWeek> daysOfTheWeek = weatherDataStartCity.getDays();
-//        tempStartDayOne.setText(startCityDayTemperatures.get(0).toString());
-//        dayOne.setText(Date.translatedDayOfTheWeek(daysOfTheWeek.get(0)));
-//        Image image = new Image(getClass().getResourceAsStream("/icons/clear-day.png"));
-//        dayOneStartImage.setImage(image);
-//    }
 
     private void updateStartingCityForecast() throws IOException, InterruptedException {
         String uriStart = Connector.buildApiRequest(startCity.getText());
@@ -88,119 +65,30 @@ public class MainWindowController extends BaseController implements Initializabl
         WeatherData weatherDataStartCity = new WeatherData(rawDataStart);
 
         List<Double> startCityDayTemperatures = weatherDataStartCity.getCityMaxTemperatures();
+        List<Double> startCityNightTemperatures = weatherDataStartCity.getCityMinTemperatures();
         List<DayOfWeek> daysOfTheWeek = weatherDataStartCity.getDays();
         List<String> icons = weatherDataStartCity.getIconTypes();
 
-        populateDaysOfTheWeek(daysOfTheWeek);
-        populateIcons(icons);
+        mainWindow.populateDaysOfTheWeek(daysOfTheWeek, weatherGrid);
+        mainWindow.populateIcons(icons, weatherGrid);
+        mainWindow.populateTemperatures(startCityDayTemperatures, startCityNightTemperatures, weatherGrid);
+
     }
 
-    private void populateDaysOfTheWeek(List<DayOfWeek> dayOfWeeks) {
-        for(int row = 0; row < weatherGrid.getRowCount(); row++) {
-            Label label = new Label();
-            String day = Date.translatedDayOfTheWeek(dayOfWeeks.get(row));
-            label.setText(day);
-            weatherGrid.add(label, 0, row);
-        }
+    private void updateDestinationCityForecast() throws IOException, InterruptedException {
+        String uriDestination = Connector.buildApiRequest(destinationCity.getText());
+        String rawDataDestination = Connector.sendRequest(uriDestination);
+        WeatherData weatherDataDestinationCity = new WeatherData(rawDataDestination);
+
+        List<Double> destinationCityDayTemperatures = weatherDataDestinationCity.getCityMaxTemperatures();
+        List<Double> destinationCityNightTemperatures = weatherDataDestinationCity.getCityMinTemperatures();
+        List<DayOfWeek> daysOfTheWeek = weatherDataDestinationCity.getDays();
+        List<String> icons = weatherDataDestinationCity.getIconTypes();
+
+        mainWindow.populateIcons(icons, weatherGrid);
+        mainWindow.populateTemperatures(destinationCityDayTemperatures, destinationCityNightTemperatures, weatherGrid);
     }
 
-    private void populateIcons(List<String> icons) {
-        for(int row = 0; row < weatherGrid.getRowCount(); row++) {
-            ImageView imageView = new ImageView();
-            String icon = icons.get(row);
-            System.out.println(icon);
-            String imagePath = getIconPath(icon);
-            System.out.println(imagePath);
-            Image image = new Image(getClass().getResourceAsStream(imagePath));
-            imageView.setImage(image);
-            weatherGrid.add(imageView, 1, row);
-        }
-    }
-
-    private String getIconPath(String icon) {
-
-        String imagePath = "/icons/";
-        switch (icon) {
-            case "clear-day":
-                imagePath += "clear-day.png";
-                break;
-            case "clear-night":
-                imagePath += "clear-night.png";
-                break;
-            case "cloudy":
-                imagePath += "cloudy.png";
-                break;
-            case "fog":
-                imagePath += "fog.png";
-                break;
-            case "hail":
-                imagePath += "hail.png";
-                break;
-            case "partly-cloudy-day":
-                imagePath += "partly-cloudy-day.png";
-                break;
-            case "partly-cloudy-night":
-                imagePath += "partly-cloudy-night.png";
-                break;
-            case "rain":
-                imagePath += "rain.png";
-                break;
-            case "rain-snow":
-                imagePath += "rain-snow.png";
-                break;
-            case "rain-snow-shower-night":
-                imagePath += "rain-snow-shower-night.png";
-                break;
-            case "rain-snow-shower-day":
-                imagePath += "rain-snow-shower-day.png";
-                break;
-            case "showers-day":
-                imagePath += "showers-day.png";
-                break;
-            case "showers-night":
-                imagePath += "showers-night.png";
-                break;
-            case "sleet":
-                imagePath += "sleet.png";
-                break;
-            case "snow":
-                imagePath += "snow.png";
-                break;
-            case "snow-showers-day":
-                imagePath += "snow-showers-day.png";
-                break;
-            case "snow-showers-night":
-                imagePath += "snow-showers-night.png";
-                break;
-            case "thunger":
-                imagePath += "thunder.png";
-                break;
-            case "thunder-rain":
-                imagePath += "thunder-rain.png";
-                break;
-            case "thunder-showers-day":
-                imagePath += "thunder-showers-day.png";
-                break;
-            case "thunder-showers-night":
-                imagePath += "thunder-showers-night.png";
-                break;
-            case "wind":
-                imagePath += "wind.png";
-                break;
-            default:
-                imagePath = imagePath;
-                break;
-        }
-        return imagePath;
-    }
-
-//    private void populateTemperatures(List<Double> startCityTemperatures) {
-//        for(int row = 0; row <= weatherGrid.getRowCount(); row++) {
-//            Text text = new Text();
-//            text.setText(startCityTemperatures.get(0).toString());
-//
-//        }
-//    }
 
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
--- WeatherApp/src/main/java/com/weather/views/MainWindow.java
+++ WeatherApp/src/main/java/com/weather/views/MainWindow.java
@@ -3,13 +3,22 @@ package com.weather.views;
 import com.weather.WeatherMenager;
 import com.weather.controller.BaseController;
 import com.weather.controller.MainWindowController;
+import com.weather.model.Date;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.time.DayOfWeek;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainWindow {
 
@@ -50,6 +59,128 @@ public class MainWindow {
         BaseController controller = new MainWindowController(weatherMenager, this,"/MainWindow.fxml");
         initializeStage(controller);
 
+    }
+
+    public void populateDaysOfTheWeek(List<DayOfWeek> dayOfWeeks, GridPane weatherGrid) {
+        for(int row = 0; row < weatherGrid.getRowCount(); row++) {
+            Label label = new Label();
+            String day = Date.translatedDayOfTheWeek(dayOfWeeks.get(row));
+            label.setText(day);
+            weatherGrid.add(label, 0, row);
+        }
+    }
+
+    public void populateIcons(List<String> icons, GridPane weatherGrid) {
+        for(int row = 0; row < weatherGrid.getRowCount(); row++) {
+            ImageView imageView = new ImageView();
+            String icon = icons.get(row);
+            String imagePath = getIconPath(icon);
+            System.out.println(imagePath);
+            Image image = new Image(getClass().getResourceAsStream(imagePath));
+            imageView.setImage(image);
+            imageView.setFitHeight(40);
+            imageView.setFitWidth(40);
+            weatherGrid.add(imageView, 1, row);
+        }
+    }
+
+    public void populateTemperatures(List<Double> dayTemperatures, List<Double> nightTemperatures, GridPane weatherGrid) {
+        for(int row = 0; row < weatherGrid.getRowCount(); row++) {
+            Label labelDayStartTemp = new Label();
+            String text = "Dzień: " + dayTemperatures.get(row).toString() + "\u00B0" + "C";
+            labelDayStartTemp.setText(text);
+            labelDayStartTemp.setMaxWidth(100);
+            labelDayStartTemp.setMaxHeight(10);
+            labelDayStartTemp.setFont(new Font(12));
+            Label labelNightStartTemp = new Label();
+
+            text = "Noc: " + nightTemperatures.get(row).toString() + "\u00B0" + "C";
+            labelNightStartTemp.setText(text);
+            labelNightStartTemp.setMaxWidth(100);
+            labelNightStartTemp.setMaxHeight(10);
+            labelNightStartTemp.setFont(new Font(12));
+            VBox vBox = new VBox();
+            vBox.getChildren().add(labelDayStartTemp);
+            vBox.getChildren().add(labelNightStartTemp);
+            weatherGrid.add(vBox,2,row);
+        }
+    }
+
+    private static String getIconPath(String icon) {
+
+        String imagePath = "/icons/";
+        switch (icon) {
+            case "clear-day":
+                imagePath += "clear-day.png";
+                break;
+            case "clear-night":
+                imagePath += "clear-night.png";
+                break;
+            case "cloudy":
+                imagePath += "cloudy.png";
+                break;
+            case "fog":
+                imagePath += "fog.png";
+                break;
+            case "hail":
+                imagePath += "hail.png";
+                break;
+            case "partly-cloudy-day":
+                imagePath += "partly-cloudy-day.png";
+                break;
+            case "partly-cloudy-night":
+                imagePath += "partly-cloudy-night.png";
+                break;
+            case "rain":
+                imagePath += "rain.png";
+                break;
+            case "rain-snow":
+                imagePath += "rain-snow.png";
+                break;
+            case "rain-snow-shower-night":
+                imagePath += "rain-snow-shower-night.png";
+                break;
+            case "rain-snow-shower-day":
+                imagePath += "rain-snow-shower-day.png";
+                break;
+            case "showers-day":
+                imagePath += "showers-day.png";
+                break;
+            case "showers-night":
+                imagePath += "showers-night.png";
+                break;
+            case "sleet":
+                imagePath += "sleet.png";
+                break;
+            case "snow":
+                imagePath += "snow.png";
+                break;
+            case "snow-showers-day":
+                imagePath += "snow-showers-day.png";
+                break;
+            case "snow-showers-night":
+                imagePath += "snow-showers-night.png";
+                break;
+            case "thunger":
+                imagePath += "thunder.png";
+                break;
+            case "thunder-rain":
+                imagePath += "thunder-rain.png";
+                break;
+            case "thunder-showers-day":
+                imagePath += "thunder-showers-day.png";
+                break;
+            case "thunder-showers-night":
+                imagePath += "thunder-showers-night.png";
+                break;
+            case "wind":
+                imagePath += "wind.png";
+                break;
+            default:
+                imagePath = null;
+                break;
+        }
+        return imagePath;
     }
 
 }
